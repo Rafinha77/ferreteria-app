2156bbe515bfc88b752f461e578169a1784c3579
Rafael hoyos <Rafael rafinha77fup@gmail.com>
2024-10-05 11:36:18 -0500

    Se crea el proyecto de ejemplo para aprender C#

---
+++ FundamentosCSharp/Proyec1/Modelos/Car.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Proyec1.Modelos
+{
+    public class Car
+    {
+        /*
+         Atributos del carro
+         */
+        string color = "";
+        int cilindraje;
+        int modelo;
+        /*
+        Metodo que retorna un mensaje 
+       */
+        public string stop()
+        {
+            string msj = "el carro está detenido ";
+            return msj;
+        }
+    }
+}
